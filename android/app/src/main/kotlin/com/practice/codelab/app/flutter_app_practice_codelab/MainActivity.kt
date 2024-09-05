51f57b0aedf35fd2957f64a0fe94a768f423c0e6
ferwisanazco <fanasco@mercantilbanco.com.pa>
2024-09-05 16:32:32 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/practice/codelab/app/flutter_app_practice_codelab/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.practice.codelab.app.flutter_app_practice_codelab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'My Second App With FLUTTER',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+  // ↓ Add this.
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners(); // notifica a todos los elementos que escuchan a myAppState
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+    var theme = Theme.of(context);
+
+    final style = theme.textTheme.displaySmall!.copyWith(
+      color: theme.colorScheme.inversePrimary,
+    );
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, //para alinear los elementos al centro
+          children: [
+            TopWelcomeText(style: style),
+            BigCard(pair: pair),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Continuar'),
+            )
+          ],
+        ),
+      ),
+
+    );
+  }
+}
+
+class TopWelcomeText extends StatelessWidget {
+  const TopWelcomeText({
+    super.key,
+    required this.style,
+  });
+
+  final TextStyle style;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Text('Bienvenido:',style: style),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+
+    var theme = Theme.of(context);
+
+    final style = theme.textTheme.displaySmall!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(100),
+        child: Text(
+            pair.asPascalCase,
+            style: style,
+            semanticsLabel: pair.asPascalCase
+        ),
+      ),
+    );
+  }
+}
